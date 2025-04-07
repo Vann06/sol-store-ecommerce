e2f9e1f9fce4bba58801bf490349f7262ca1ab08
Vann06 <cas23201@uvg.edu.gt>
2025-04-06 22:40:50 -0600

    Update web.php

--- taskcurso/routes/web.php
+++ taskcurso/routes/web.php
@@ -11,10 +11,4 @@ Route::get('/', function () {
 // Admin panel para productos
 Route::resource('admin/products', ProductController::class)->names('admin.products');
 
-Route::get('/admin/products/create', [ProductController::class, 'create'])->name('admin.products.create');
-
-Route::post('/admin/products', [ProductController::class, 'store'])->name('admin.products.store');
-
-// Rutas REST del recurso
-Route::resource('products', ProductController::class);
 
