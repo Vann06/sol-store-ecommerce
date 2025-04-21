8116992c38b077852c07c6a24b9542546f72418f
Cain7564 <156337459+Cain7564@users.noreply.github.com>
2025-04-21 17:55:52 -0600

    Update HomeView.vue

--- front-vue/src/views/HomeView.vue
+++ front-vue/src/views/HomeView.vue
@@ -9,6 +9,10 @@
         <FeaturedCategories />
         <FeaturedThemes />
         <FeaturedProducts />
+        <FeaturedBenefits />
+        <FeaturedBest />
+        <FeaturedSpecial />
+        <FeaturedDio />
       </main>
   
     </div>
@@ -16,10 +20,14 @@
   
   <script setup>
   import Header from '@/components/Header.vue'
+  import FeaturedBenefits from '@/components/Home/FeaturedBenefits.vue'
   import HeroSlider from '@/components/Home/HeroSlider.vue'
   import FeaturedCategories from '@/components/Home/FeaturedCategories.vue'
   import FeaturedThemes from '@/components/Home/FeaturedThemes.vue'
   import FeaturedProducts from '@/components/Home/FeaturedProducts.vue'
+  import FeaturedBest from '@/components/Home/FeaturedBest.vue'
+  import FeaturedSpecial from '@/components/Home/FeaturedSpecial.vue'
+  import FeaturedDio from '@/components/Home/FeaturedDio.vue'
   </script>
   
   <style scoped>
