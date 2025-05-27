ac517827d6e8af917e442f0b1055d6b82e2f37ce
Vann06 <cas23201@uvg.edu.gt>
2025-05-27 08:21:21 -0600

    App & main authentication

--- front-vue/src/main.js
+++ front-vue/src/main.js
@@ -1,25 +1,24 @@
-import './assets/main.css'
-import './assets/css/bootstrap.min.css'
-import './assets/css/font-awesome.min.css'
-import './assets/css/elegant-icons.css'
-import './assets/css/owl.carousel.min.css'
-import './assets/css/slicknav.min.css'
-import './assets/css/style.css'
-
 import { createApp } from 'vue'
-import '@fortawesome/fontawesome-free/css/all.min.css'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
-const app = createApp(App)
+// Axios setup
+axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.withCredentials = true
+const token = localStorage.getItem('auth_token')
+if (token) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
 
+// Crear instancia de Vue y Pinia correctamente
+const app = createApp(App)
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
-app.use(createPinia())
-app.use(router)
 app.use(pinia)
+app.use(router)
 
 app.mount('#app')
